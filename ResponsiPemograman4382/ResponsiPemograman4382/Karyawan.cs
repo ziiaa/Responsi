5b1ce9d3f53e723d72fd8d8b3a822b12d2a97d95
ziiaa <97387524+ziiaa@users.noreply.github.com>
2022-04-19 11:56:23 +0700

    Add files via upload

---
+++ ResponsiPemograman4382/ResponsiPemograman4382/Karyawan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemograman4382
+{
+	class Karyawan
+	{
+		public int Nik { get; set; }
+		public string Nama { get; set; }
+		public int Gaji { get; set; }
+		public int GajiNaik { get; set; }
+
+
+
+		public Karyawan(int NIK, string NAMA, int GajiBiasa)
+		{
+			Nik = NIK;
+			Nama = NAMA;
+			Gaji = GajiBiasa;
+			GajiNaik = 10;
+
+			if (GajiBiasa < 0)
+			{
+				Gaji = 0;
+			}
+		}
+
+		public void getBonus()
+		{
+			int Bonus = Gaji / GajiNaik;
+			Gaji = Gaji + Bonus;
+			Console.WriteLine("{0}   {1} {2}", Nik, Nama, Gaji);
+		}
+
+
+
+		public void PrintAndShow()
+		{
+			Console.WriteLine("{0}  {1} {2}", Nik, Nama, Gaji);
+		}
+
+
+
+
+	}
+}
